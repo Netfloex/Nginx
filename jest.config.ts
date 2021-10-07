12ad2a73b1092a243d930480c2ec65c8e041f4fd
Netfloex <netfloex@gmail.com>
2021-10-08 00:43:15 +0200

    Added rollup, removed ts-patch and ts-transform

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
+import { compilerOptions } from "./tsconfig.json";
 import { InitialOptionsTsJest } from "ts-jest/dist/types";
+import { pathsToModuleNameMapper } from "ts-jest/utils";
 
 const options: InitialOptionsTsJest = {
 	preset: "ts-jest",
@@ -7,7 +9,10 @@ const options: InitialOptionsTsJest = {
 	snapshotFormat: {
 		printBasicPrototype: false
 	},
-	clearMocks: true
+	clearMocks: true,
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+		prefix: "../../"
+	})
 };
 
 export default options;
---
+++ rollup.config.ts
@@ -0,0 +1,38 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { defineConfig } from "rollup";
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+
+export default defineConfig({
+	input: "src/index.ts",
+	output: {
+		dir: "dist",
+		format: "cjs",
+		generatedCode: "es5",
+		plugins: [terser()]
+	},
+	external: [
+		"fs-extra",
+		"path",
+		"axios",
+		"chalk",
+		"url",
+		"zod",
+		"crypto",
+		"dns",
+		"fs",
+		"glob",
+		"clean-css"
+	],
+	plugins: [
+		typescript({
+			exclude: ["src/tests"],
+			tsconfigOverride: {
+				compilerOptions: {
+					module: "ESNext"
+				}
+			}
+		}),
+		commonjs()
+	]
+});
