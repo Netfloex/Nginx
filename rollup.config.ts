f61ef990b78f0845123546fc693eef382e8745f5
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-04-15 15:31:07 +0200

    Fix cert2json for loop using @rollup/replace

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import { defineConfig } from "rollup";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
@@ -13,6 +14,16 @@ export default defineConfig({
 		plugins: [terser()]
 	},
 	plugins: [
+		replace({
+			// The package cert2json does not instantiate the variable `i` which strict mode or rollup does not like
+			// So this fixes it temporarily
+			values: {
+				"for (i = 0;": "for (let i = 0;"
+			},
+			delimiters: ["", ""],
+			include: "**/cert2json/src/der.js",
+			preventAssignment: true
+		}),
 		typescript({
 			exclude: ["src/tests"]
 		}),
