97123e045b17f6c51796db0d79e0002ae6ec89eb
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-04-18 15:38:05 +0200

    Updated cert2json, removed @rollup/replace

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,5 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
 import { defineConfig } from "rollup";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
@@ -14,16 +13,6 @@ export default defineConfig({
 		plugins: [terser()]
 	},
 	plugins: [
-		replace({
-			// The package cert2json does not instantiate the variable `i` which strict mode or rollup does not like
-			// So this fixes it temporarily
-			values: {
-				"for (i = 0;": "for (let i = 0;"
-			},
-			delimiters: ["", ""],
-			include: "**/cert2json/src/der.js",
-			preventAssignment: true
-		}),
 		typescript({
 			exclude: ["src/tests"]
 		}),
