bb33eb90a190ac146091a0a55825336026f54190
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-09-05 23:40:45 +0200

    Automatically renew certificates each 8 days

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,36 @@
 PREFIX="[NCM] (entrypoint.sh)"
 
 start() {
-    echo "$PREFIX Generating configs"
-    node index.js run
+    echo "$PREFIX Generating configs..."
+    node index run
+}
+
+reload_nginx() {
+    echo "$PREFIX Reloading nginx..."
+    nginx -s reload && echo "$PREFIX Nginx reloaded"
 }
 
 reload() {
     if start; then
-        echo "$PREFIX Reload nginx"
-        nginx -s reload && echo "$PREFIX Nginx reloaded"
-        wait -n $NGINX
+        reload_nginx
     fi
 }
 
+RENEWAL_INTERVAL=8d
+
+start_renew_loop() {
+    while [ true ]; do
+        echo "$PREFIX Certificate renewal will now sleep for $RENEWAL_INTERVAL... "
+        sleep $RENEWAL_INTERVAL
+        echo "$PREFIX Renewing certificates..."
+        node index certificates
+    done
+}
+
 clean_exit() {
+    echo "$PREFIX Exiting..."
     nginx -s stop
+    kill $LOOP_PID
 }
 
 if [[ ! -d "$NGINX_CONFIG_PATH" ]]; then
@@ -45,18 +61,30 @@ if [ ! -z "$STANDALONE" ];then
     exit
 fi
 
-nginx -g "daemon off;" & NGINX=$!
-
 trap exit TERM INT QUIT
 trap clean_exit EXIT
 trap reload SIGHUP
 
+echo "$PREFIX Starting Nginx..."
+nginx -g "daemon off;" & NGINX_PID=$!
+
+
 if start; then
-    echo "$PREFIX Reloading Nginx"
-    nginx -s reload
+    reload_nginx
 fi
 
+start_renew_loop & LOOP_PID=$!
 
 
-wait $NGINX
+# Waiting stops whenever the HUP signal is sent
+# This is why we need to wait in a loop if:
+#	- It is the first iteration (EXIT_CODE is still empty)
+#   - The HUP signal was sent
+
+while [ -z "${EXIT_CODE}" ] || [ "${EXIT_CODE}" = "129" ]; do
+    wait -n ${NGINX_PID} ${LOOP_PID}
+    EXIT_CODE=$?
+done
+
+echo "$PREFIX Program exited"
 exit $?
